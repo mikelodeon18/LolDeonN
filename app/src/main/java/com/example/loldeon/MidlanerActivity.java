8de15533622c36482102ab18817ae15144d69a66
MiguelLucíaT <miguelin luc2014@gmail.com>
2023-11-13 12:29:58 +0100

    LOLDEON

--- app/src/main/java/com/example/loldeon/MidlanerActivity.java
+++ app/src/main/java/com/example/loldeon/MidlanerActivity.java
@@ -1,14 +1,15 @@
+
 package com.example.loldeon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MidlanerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.paginalogin);
+        setContentView(R.layout.activity_midlaner);
     }
 }
